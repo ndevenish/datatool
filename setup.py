d3c4ab5137141716a9bc7ec28c04cd58726ea218
Nicholas Devenish <ndevenish@gmail.com>
2014-12-18 18:28:56 +0000

    Initial skeleton structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+import setuptools
+
+setup(name="data",
+      version='0.1',
+      author="Nicholas Devenish",
+      author_email="ndevenish@gmail.com",
+      packages=['data'],
+      scripts=['bin/data'],
+      requires=["docopt"],
+     )
