b49180572a566151486ac94281870fb8739b911c
Nicholas Devenish <ndevenish@gmail.com>
2015-01-06 19:10:16 +0000

    Add a tag-attribute based navigation

--- datatool/toolinterface.py
+++ datatool/toolinterface.py
@@ -1,4 +1,5 @@
 import os
+import itertools
 
 from .index import find_index, LocalFileIndex
 from .authority import find_authority, LocalFileAuthority
@@ -7,15 +8,64 @@ from .util import first
 class MissingDatafileError(IOError):
   pass
 
+class SubsetError(IndexError):
+  pass
+
+class DataSetFileNavigator(object):
+  def __init__(self, dataset, subset, path=None):
+    """Initialise for a specific dataset, with subset narrowed"""
+    if len(subset) == 0:
+      raise SubsetError("Indexed subset contains no entries")
+    self._dataset = dataset
+    self._subset = subset
+    self._path = path or []
+    self._tags = set([str(x) for x in itertools.chain(*[x.tags for x in self._subset])])
+    self._extensions = [os.path.splitext(x.get_valid_instance().filename)[1].lstrip(".").lower() for x in self._subset]
+
+  @property
+  def all(self):
+    return [x.get_valid_instance().filename for x in self._subset]
+  
+  @property
+  def only(self):
+    if len(self._subset) > 1:
+      raise SubsetError("More than one entry for selected subset; {}".format(",".join(self.all)))
+    return first(self._subset).get_valid_instance().filename
+
+  @property
+  def tags(self):
+    return self._tags
+
+  def __getattr__(self, attr):
+    """Allow addressing via tag"""
+    attr = attr.lower()
+    path = self._path + [attr]
+    if attr in [x.lower() for x in self._tags]:
+      return DataSetFileNavigator(self._dataset, [x for x in self._subset if attr in [y.lower() for y in x.tags]], path)
+    elif attr in [x.lower() for x in self._extensions]:
+      return DataSetFileNavigator(self._dataset, [x for x in self._subset if x.get_valid_instance().filename.lower().endswith(attr)], path)
+    else:
+      raise SubsetError("No entries in subset with tag or extension named '{}'".format(attr))
+
+  def __str__(self):
+    return "<DataSetFileNavigator {}.{}>".format(self._dataset.__display_str__(), ".".join(self._path))
+  def __repr__(self):
+    return self.__str__()
+  def __len__(self):
+    return len(self._subset)
+  def __iter__(self):
+    return iter(self.all)
+
 class DatasetInterface(object):
   """An interface to data sets, to be handed to the python user"""
   def __init__(self, dataset):
     self._dataset = dataset
 
+
   def _filenames(self):
     for filei in self._dataset.files:
       # work backwards and find the first valid instance
-      valid = first([x for x in reversed(filei.instances) if os.path.isfile(x.filename)])
+      valid = filei.get_valid_instance()
       if valid:
         yield valid.filename
       elif not filei.instances:
@@ -23,12 +73,19 @@ class DatasetInterface(object):
       else:
         raise MissingDatafileError("Could not find instance of file in [{}]".format(", ".join(x.filename for x in filei.instances)))
 
+  def __display_str__(self):
+    return self._dataset.name or self._Dataset.id[:5]
+
   def __str__(self):
-    return "<Dataset '{}', {} files>".format(self._dataset.name or self._Dataset.id[:5], len(self._dataset.files))
+    return "<Dataset '{}', {} files>".format(self.__display_str__(), len(self._dataset.files))
   def __repr__(self):
     return str(self)
   def __iter__(self):
     return iter(self._filenames())
+  def __getattr__(self, name):
+    """Access files via tag."""
+    return getattr(DataSetFileNavigator(self, self._dataset.files), name)
+
 
 class Datatool(object):
   def __init__(self):
@@ -39,6 +96,8 @@ class Datatool(object):
   def get_dataset(self, name_or_id):
     """Retrieves a particular dataset"""
     dset = self._authority.fetch_dataset(name_or_id)
+    if not dset:
+      raise IndexError("Could not find dataset entry for " + name_or_id)
     return DatasetInterface(dset)
 
   def get_file(self, name_or_id):
